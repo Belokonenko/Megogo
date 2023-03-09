1094a39173d49215ed3f725e1914bd1e6b79f32a
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-03-09 04:24:58 +0200

    fix line hit slider'

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 import baner from "../parts/baner/baner.js";
+import hit from "../parts/hit/hit.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     baner();
+    hit();
 })
