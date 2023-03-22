500d7b95088c8b85043d788bbb9e74cecda2336e
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-03-22 07:49:32 +0200

    pag sort

--- src/parts/newest/newest.js
+++ src/parts/newest/newest.js
@@ -4,7 +4,9 @@ export default function newest() {
     const newestContent = document.querySelector(".newest__content");
 
     if (newestContent) {
+        let dataSort = dataContent;
         
+
         // --- pagination
 
         const btnPrev = document.querySelector(".pag-btn-prev");
@@ -12,24 +14,19 @@ export default function newest() {
 
         const itemsPerPage = 20;
         const pages = Math.ceil(dataContent.length / itemsPerPage);
-        const maxPag = 3;
         let currentPage = 1;
 
         const pagContainer = document.querySelector(".newest__pagination-num");
-        
-        displayContent();
-        // displayData(dataContent, currentPage, itemsPerPage);
-        // displayPagination();
+
+        displayContent(dataSort);
 
         // --- listeners btn pagination
 
         btnNext.addEventListener("click", () => {
             ++currentPage;
-            
+
             if (currentPage <= pages) {
-                displayContent();
-                // displayData(dataContent, currentPage, itemsPerPage);
-                // displayPagination();
+                displayContent(dataSort);
             } else {
                 currentPage = pages;
             }
@@ -37,24 +34,21 @@ export default function newest() {
 
         btnPrev.addEventListener("click", () => {
             --currentPage;
-            
+
             if (currentPage > 0) {
-                displayContent();
-                // displayData(dataContent, currentPage, itemsPerPage);
-                // displayPagination();
+                displayContent(dataSort);
             } else {
                 currentPage = 1;
             }
-
         });
-        
+
         // --- /listeners btn pagination
 
-        // --- functions
-        
-        function displayContent() {
-                displayData(dataContent, currentPage, itemsPerPage);
-                displayPagination();
+        // --- functions pagination
+
+        function displayContent(data) {
+            displayData(data, currentPage, itemsPerPage);
+            displayPagination();
         }
 
         function getPagBtn(i) {
@@ -63,8 +57,7 @@ export default function newest() {
             btn.addEventListener("click", () => {
                 console.log(`click ${i}`);
                 currentPage = i;
-                displayData(dataContent, currentPage, itemsPerPage);
-                displayPagination();
+                displayContent(dataSort);
             });
 
             btn.classList.add("newest__pag-num");
@@ -91,7 +84,7 @@ export default function newest() {
             pagContainer.innerHTML = "";
 
             if (currentPage == 1) {
-                addBtnPag(getPagBtn(currentPage ));
+                addBtnPag(getPagBtn(currentPage));
                 addBtnPag(getPagBtn(currentPage + 1));
                 addBtnPag(getPagBtn(currentPage + 2));
                 setDots();
@@ -163,125 +156,101 @@ export default function newest() {
             });
         }
 
-        // --- pagination
+        // --- pagination pagination
+        
+        // --- sort
+
+        const btn = document.querySelector(".newest__btn");
+        const arr = document.querySelector(".newest__btn-arr");
+        const nameBtn = document.querySelector(".sort-name");
+        const dateBtn = document.querySelector(".sort-date");
+        const ratingBtn = document.querySelector(".sort-rating");
+        const dropList = document.querySelector(".newest__drop-list");
+        const wrapBtn = document.querySelector(".newest__wrap-btn");
+
+        // --- listeners sort
+
+        btn.addEventListener("click", () => {
+            dropList.classList.toggle("newest__drop-list--active");
+            arr.classList.toggle("newest__btn-arr--active");
+        });
+
+        wrapBtn.addEventListener("mouseleave", () => {
+            dropList.classList.remove("newest__drop-list--active");
+            arr.classList.remove("newest__btn-arr--active");
+        });
+
+        nameBtn.addEventListener("click", () => {
+            dataSort = sortDesc(dataContent);
+            currentPage = 1;
+            displayContent(dataSort);
+        });
+
+        dateBtn.addEventListener("click", () => {
+            dataSort = sortDate(dataContent);
+            currentPage = 1;
+            displayContent(dataSort);
+        });
+
+        ratingBtn.addEventListener("click", () => {
+            dataSort = sortRating(dataContent);
+            currentPage = 1;
+            displayContent(dataSort);
+        });
+
+        // --- /listeners sort
+
+        // --- functions sort
+
+        function sortDesc(arr) {
+            const temp = JSON.parse(JSON.stringify(arr));
+
+            // sort by name
+            temp.sort((a, b) => {
+                const nameA = a.desc.toUpperCase(); // ignore upper and lowercase
+                const nameB = b.desc.toUpperCase(); // ignore upper and lowercase
+
+                if (nameA < nameB) {
+                    return -1;
+                }
+
+                if (nameA > nameB) {
+                    return 1;
+                }
+
+                return 0;
+            });
+            return temp;
+        }
+
+        function sortDate(arr) {
+            const temp = JSON.parse(JSON.stringify(arr));
+
+            temp.sort((a, b) => {
+                const ratingA = +a.year;
+                const ratingB = +b.year;
+
+                return ratingA - ratingB;
+            }).reverse();
+
+            return temp;
+        }
+
+        function sortRating(arr) {
+            const temp = JSON.parse(JSON.stringify(arr));
+
+            temp.sort((a, b) => {
+                const ratingA = +a.rating;
+                const ratingB = +b.rating;
+
+                return ratingA - ratingB;
+            }).reverse();
+
+            return temp;
+        }
+
+        // --- /functions sort
+
+        // --- /sort
     }
 }
-
-// setContent(dataContent);
-
-// // -- sort
-
-// const btn = document.querySelector(".newest__btn");
-// const arr = document.querySelector(".newest__btn-arr");
-// const nameBtn = document.querySelector(".sort-name");
-// const dateBtn = document.querySelector(".sort-date");
-// const ratingBtn = document.querySelector(".sort-rating");
-// const dropList = document.querySelector(".newest__drop-list");
-// const wrapBtn = document.querySelector(".newest__wrap-btn");
-
-// btn.addEventListener("click", () => {
-//     dropList.classList.toggle("newest__drop-list--active");
-//     arr.classList.toggle("newest__btn-arr--active");
-// });
-// wrapBtn.addEventListener("mouseleave", () => {
-//     dropList.classList.remove("newest__drop-list--active");
-//     arr.classList.remove("newest__btn-arr--active");
-// });
-
-// nameBtn.addEventListener("click", () => {
-//     setContent(sortDesc(dataContent));
-// });
-
-// dateBtn.addEventListener("click", () => {
-//     setContent(sortDate(dataContent));
-// });
-
-// ratingBtn.addEventListener("click", () => {
-//     setContent(sortRating(dataContent));
-// });
-
-// // -- /sort
-
-// // -- functions
-
-// function sortDesc(arr) {
-//     const temp = JSON.parse(JSON.stringify(arr));
-
-//     // sort by name
-//     temp.sort((a, b) => {
-//         const nameA = a.desc.toUpperCase(); // ignore upper and lowercase
-//         const nameB = b.desc.toUpperCase(); // ignore upper and lowercase
-//         if (nameA < nameB) {
-//             return -1;
-//         }
-//         if (nameA > nameB) {
-//             return 1;
-//         }
-//         // names must be equal
-//         return 0;
-//     });
-//     return temp;
-// }
-
-// function sortDate(arr) {
-//     const temp = JSON.parse(JSON.stringify(arr));
-
-//     temp.sort((a, b) => {
-//         const ratingA = +a.year;
-//         const ratingB = +b.year;
-
-//         return ratingA - ratingB;
-//     }).reverse();
-
-//     return temp;
-// }
-
-// function sortRating(arr) {
-//     const temp = JSON.parse(JSON.stringify(arr));
-
-//     temp.sort((a, b) => {
-//         const ratingA = +a.rating;
-//         const ratingB = +b.rating;
-
-//         return ratingA - ratingB;
-//     }).reverse();
-
-//     return temp;
-// }
-
-// function setContent(arr) {
-//     newestContent.innerHTML = "";
-
-//     arr.forEach((item, i) => {
-//         newestContent.innerHTML += `
-//         <a class="card-b" href=${item.href} data-num=${i}>
-//             <div class="card-b__wrap-img">
-//                 <img
-//                     class="card-b__img"
-//                     src=${item.img}
-//                     alt="movie card-b"
-//                 />
-//                 <div class="card-b__resolution">
-//                     <p class="card-b__resolution-num">${
-//                         item.resolution
-//                     }</p>
-//                     <p class="card-b__resolution-pix">p</p>
-//                 </div>
-//                 <div class="card-b__rating">
-//                     <p class="card-b__rating-num">${item.rating}</p>
-//                 </div>
-//             </div>
-//             <div class="card-b__wrap-time">
-//                 <span class="card-b__year">${item.year}</span>
-//                 <span class="card-b__time">${item.time}p</span>
-//             </div>
-//             <div >
-//                 <span class="card-b__name">${item.name}</span>
-//                 <span>${" : "}</span>
-//                 <span class="card-b__desc">${item.desc}</span>
-//             </div>
-//         </a>
-
-//     `;
-//     });
-// }
