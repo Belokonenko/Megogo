7afc2a60beb898b0c63e58abb97ea3cef0992d0d
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-03-25 16:40:16 +0200

    search done

---
+++ src/parts/search/search.js
@@ -0,0 +1,23 @@
+import { dataContent } from "../bd/bd.js";
+
+export default function search() {
+    const listResult = document.querySelector(".search__res-list");
+    const searchInput = document.querySelector(".search__input");
+
+    searchInput.addEventListener("input", () => {
+        const value = searchInput.value.trim();
+        listResult.innerHTML = "";
+        if (value) {
+            dataContent.forEach((item) => {
+                if ((item.name.toLowerCase().search(value.toLowerCase())) > -1) {
+                    console.log(value);
+                    listResult.innerHTML += `
+                        <li class="search__res-item"><a href="./movie.html">${item.name}</a></li>
+
+                    `;
+                }
+            });
+        }
+    });
+
+}
