862d87af0156fe417704a689d1f1f05d30850f09
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-03-26 15:15:09 +0300

    fix errors

--- src/parts/search/search-b.js
+++ src/parts/search/search-b.js
@@ -1,24 +1,25 @@
 import { dataContent } from "../bd/bd.js";
 
-export default function searchB() { 
-
-    const listResult = document.querySelector(".search-b__res-list");
+export default function searchB() {
     const searchInput = document.querySelector(".search-b__input");
+    const listResult = document.querySelector(".search-b__res-list");
 
-    searchInput.addEventListener("input", () => {
-        const value = searchInput.value.trim();
-        listResult.innerHTML = "";
-        if (value) {
-            dataContent.forEach((item) => {
-                if ((item.name.toLowerCase().search(value.toLowerCase())) > -1) {
-                    listResult.innerHTML += `
+    if (searchInput) {
+        searchInput.addEventListener("input", () => {
+            const value = searchInput.value.trim();
+            listResult.innerHTML = "";
+            if (value) {
+                dataContent.forEach((item) => {
+                    if (
+                        item.name.toLowerCase().search(value.toLowerCase()) > -1
+                    ) {
+                        listResult.innerHTML += `
                         <li class="search__res-item"><a href="./movie.html">${item.name}</a></li>
 
                     `;
-                }
-            });
-        }
-    });
-
-};
-    
+                    }
+                });
+            }
+        });
+    }
+}
