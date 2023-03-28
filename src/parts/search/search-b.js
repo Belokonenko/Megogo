b5f9ee23bb28a6aa820163503d9a1b42d156a4ce
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-03-28 15:29:40 +0300

    add close list search

--- src/parts/search/search-b.js
+++ src/parts/search/search-b.js
@@ -1,18 +1,29 @@
 import { dataContent } from "../bd/bd.js";
 
 export default function searchB() {
+    const searchB = document.querySelector(".search-b");
     const searchInput = document.querySelector(".search-b__input");
     const listResult = document.querySelector(".search-b__res-list");
+
+    searchB.addEventListener("mouseleave", () => {
+        listResult.innerHTML = "";
+        searchInput.value = "";
+    });
 
     if (searchInput) {
         searchInput.addEventListener("input", () => {
             const value = searchInput.value.trim();
+
             listResult.innerHTML = "";
+
+            let num = 0;
+
             if (value) {
                 dataContent.forEach((item) => {
-                    if (
-                        item.name.toLowerCase().search(value.toLowerCase()) > -1
-                    ) {
+                    const val = value.toLowerCase();
+
+                    if (item.name.toLowerCase().search(val) > -1 && num < 10) {
+                        num++;
                         listResult.innerHTML += `
                         <li class="search__res-item"><a href="./movie.html">${item.name}</a></li>
 
--- src/parts/search/search.js
+++ src/parts/search/search.js
@@ -1,15 +1,30 @@
 import { dataContent } from "../bd/bd.js";
 
 export default function search() {
+    const formSearch = document.querySelector(".search");
+
     const listResult = document.querySelector(".search__res-list");
     const searchInput = document.querySelector(".search__input");
 
+    formSearch.addEventListener("mouseleave", () => {
+        listResult.innerHTML = "";
+        searchInput.value = "";
+    });
+
     searchInput.addEventListener("input", () => {
         const value = searchInput.value.trim();
+        
         listResult.innerHTML = "";
+        
         if (value) {
+            let num = 0;
+            
             dataContent.forEach((item) => {
-                if ((item.name.toLowerCase().search(value.toLowerCase())) > -1) {
+                const val = value.toLowerCase();
+                
+                if (item.name.toLowerCase().search(val) > -1 && num < 10) {
+                    num++;
+                    
                     listResult.innerHTML += `
                         <li class="search__res-item"><a href="./movie.html">${item.name}</a></li>
 
@@ -18,5 +33,4 @@ export default function search() {
             });
         }
     });
-
 }
